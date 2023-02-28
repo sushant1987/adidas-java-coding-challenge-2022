c75ab9267fe565d19e5a5da101a3e5a76f6848c8
Sushant Kumar Singh <sushant1887@gmail.com>
2023-02-28 20:37:55 +0100

    swagger documentation

--- public-service/src/main/java/com/adidas/backend/publicservice/controller/SubscribtionRestController.java
+++ public-service/src/main/java/com/adidas/backend/publicservice/controller/SubscribtionRestController.java
@@ -17,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.adidas.backend.publicservice.dto.SubscriptionUser;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 /**
  * This controller provides REST API for subscription
  * 
@@ -44,6 +49,10 @@ public class SubscribtionRestController {
 	 * @param user
 	 * @return
 	 */
+	@Operation(summary = "Registeration API for priority sales")
+	@ApiResponses(value = {@ApiResponse(responseCode = "202", description = "Accepted for registartion",
+	content = @Content(mediaType = "text/plain;charset=UTF-8")), @ApiResponse(responseCode = "400", description = 
+	"Bad Resquest", content = @Content(mediaType = "text/plain;charset=UTF-8"))})
 	@PostMapping
 	public ResponseEntity<String> createSubscription(@Valid @RequestBody SubscriptionUser user) {
 		log.info("Subscription initiated for {}", user.getEmail());
--- public-service/src/main/java/com/adidas/backend/publicservice/exception/ExceptionHandlerController.java
+++ public-service/src/main/java/com/adidas/backend/publicservice/exception/ExceptionHandlerController.java
@@ -3,11 +3,13 @@
  */
 package com.adidas.backend.publicservice.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Controller for handling exceptions
@@ -26,6 +28,7 @@ public class ExceptionHandlerController {
 	 * @return
 	 */
 	@ExceptionHandler(value = MethodArgumentNotValidException.class)
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 	public ResponseEntity<String> badRequest(MethodArgumentNotValidException ex) {
 		
 		return ResponseEntity.badRequest().body(buildErrorMessage(ex));
