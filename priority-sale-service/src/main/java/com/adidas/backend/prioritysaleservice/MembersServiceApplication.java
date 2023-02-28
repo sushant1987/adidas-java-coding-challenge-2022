c398a31f97a959de27a14a4a920af9c48fbff1ae
Sushant Kumar Singh <sushant1887@gmail.com>
2023-02-28 19:44:25 +0100

    priority sales service configuration

--- priority-sale-service/src/main/java/com/adidas/backend/prioritysaleservice/MembersServiceApplication.java
+++ priority-sale-service/src/main/java/com/adidas/backend/prioritysaleservice/MembersServiceApplication.java
@@ -1,13 +1,29 @@
 package com.adidas.backend.prioritysaleservice;
 
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableKafka
+@EnableScheduling
 public class MembersServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MembersServiceApplication.class, args);
 	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 
+	    return builder.setConnectTimeout(Duration.ofMillis(300000))
+	     .setReadTimeout(Duration.ofMillis(600000)).build();
+	}
+	
 }
